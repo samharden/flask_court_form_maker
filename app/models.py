dc131233c2316107e44c3f496d11b510e0f064cf
samharden <samharden@gmail.com>
2016-07-28 15:24:31 -0400

    updated the models.py file to limit table display columns

--- app/models.py
+++ app/models.py
@@ -5,7 +5,10 @@ from app import engine
 def search_last(last):
     sql_text = 'SELECT * FROM odyssey WHERE lastname_stripped LIKE "%' + last + '%";'
     df = pd.read_sql_query(sql_text, engine)
-    return df
+    
+    
+    
+    return dfr
 
 
 def search_all(first, last, case):
@@ -34,6 +37,11 @@ def search_all(first, last, case):
     if len(df) == 0:
         print "Need to add NO RESULTS prompt and return to index"
     print 'Done with search'
+    
+    
+    df.drop(['last_name', 'firstname_stripped', 'first_name', 'lastname_stripped', 'index'], axis=1, inplace=True)
+    #df.drop ?? in pandas  df.drop('name of column', axis=1 **where it is)
+    
     return df
 
 
